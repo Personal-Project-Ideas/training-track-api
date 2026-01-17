f1ade0452efb6959a04a29b76321fc82b79728a6
Marcelo Prates <mpratesjunior@gmail.com>
2026-01-16 22:25:51 -0300

    feat(#1): add env  vars

--- internal/bootstrap/composition/infraestructure/infra_composer.go
+++ internal/bootstrap/composition/infraestructure/infra_composer.go
@@ -2,17 +2,26 @@ package infraestructure_composer
 
 import (
 	"github.com/PratesJr/training-track-api/internal/domain/ports"
+	"github.com/PratesJr/training-track-api/internal/infra/persistences"
+	"github.com/PratesJr/training-track-api/internal/pkg/database"
 	"gorm.io/gorm"
 )
 
 type InfraContainer struct {
 	UserRepo ports.UserRepository
-	Db       *gorm.DB
 }
 
 func Compose() *InfraContainer {
 
-	infraContainer := &InfraContainer{}
+	var db *gorm.DB
+	var userRepo ports.UserRepository
+
+	db = database.DbConnection()
+	userRepo = persistences.UserPersistence(db)
+
+	infraContainer := &InfraContainer{
+		UserRepo: userRepo,
+	}
 
 	return infraContainer
 }
